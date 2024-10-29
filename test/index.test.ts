de4e24d9137883e40eb02ba8108beeeb2c9063c4
Mark Wylde <me@markwylde.com>
2024-10-29 18:13:03 +0000

    chore: add more unique test stream names

--- test/index.test.ts
+++ test/index.test.ts
@@ -8,7 +8,7 @@ describe('Eventbase', async () => {
   let eventbase2;
 
   beforeEach(async () => {
-    const streamName = `test-${Date.now()}`;
+    const streamName = `test-${Date.now()}-${Math.random().toString(36).substring(7)}`;
     [eventbase1, eventbase2] = await Promise.all([
       createEventbase({
         servers: ['localhost:4222'],
